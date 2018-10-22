0ec613e1b34c46c91d74947e3a94107c584a0b9c
Eloi Strée <jams.center@gmail.com>
2018-10-22 22:57:48 +0200

    The code of Hack in Borgenhout is so quickly and dirty code. It needs to be remover to restart with clean code.

--- TapEloiToolBox/TapWithUs Toolbox Eloi Standard/Assets/Painter/PaintV2/Script/Core/PainterCheckData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class PainterCheckData :ScriptableObject{
-	public bool twoDOrThreeD = true;
-	public Vector2 gridSize;
-	public List<Vector2> checkPoints;
-}
